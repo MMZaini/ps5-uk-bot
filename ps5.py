7b24e3e1489f44aacf81cb8d08ba4ead9b1b78dc
Mahdi Zaini <73606041+MMZaini@users.noreply.github.com>
2021-01-20 10:43:09 +0000

    Add files via upload

--- ps5.py
+++ ps5.py
@@ -3,20 +3,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import winsound
 
+item = 'https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-digital-edition-console/p/191430' # Variable for the item wanted
 options = Options()
 options.add_argument("user-data-dir=C:\\Users\\ps5\\AppData\\Local\\Google\\Chrome\\User Data")
 driver = webdriver.Chrome('chromedriver.exe', options=options)
-driver.get('https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-digital-edition-console/p/191430')
+driver.get(item)
 driver.maximize_window()
 while True:
     try:
-        #code with possible error
-        driver.find_element_by_xpath('//*[@id="addToCartButton"]').click() #look for add to cart button
+        # Code with possible error - error being that there's no add to cart button
+        driver.find_element_by_xpath('//*[@id="addToCartButton"]').click() # Looks for the 'add to cart button' using xpath
     except:
-        driver.get('https://www.smythstoys.com/uk/en-gb/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-digital-edition-console/p/191430') #if button not there, refresh page
+        driver.get(item) # If the atc button cannot be found, refresh until it is found
     else:
-        #the rest of the code
         time.sleep(1)
-        driver.get('https://www.smythstoys.com/uk/en-gb/checkout/multi/payment-method/add') #go to fulfillment page after clicking add to cart
+        driver.get('https://www.smythstoys.com/uk/en-gb/checkout/multi/payment-method/add') # Goes to checkout page after adding to cart
         break
-winsound.Beep(800, 5000)
+winsound.Beep(800, 5000) # Makes a beep sound for 5 seconds to notify the user that the item is in stock and at checkout page
