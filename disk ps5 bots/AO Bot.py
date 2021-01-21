4c9da0a5190bd7befd9651e8aaa2d2bb503fea2b
MMZaini <73606041+MMZaini@users.noreply.github.com>
2021-01-21 10:35:17 +0000

    add disk and digital folders
    add disk and digital folders

--- disk ps5 bots/AO Bot.py
+++ disk ps5 bots/AO Bot.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import winsound
 
-print("PS5 Bot Copyright (C) 2021  Mahdi Zaini This program comes with ABSOLUTELY NO WARRANTY; for details check included license. This is free software, and you are welcome to redistribute it under certain conditions; check license for conditions.")
+print("PS5 UK Bot Copyright (C) 2021  Mahdi Zaini This program comes with ABSOLUTELY NO WARRANTY; for details check included license. This is free software, and you are welcome to redistribute it under certain conditions; check license for conditions.")
 
 item = 'https://ao.com/product/p5hehwsny39500-sony-playstation-playstation-5-console-white-79528-291.aspx' # Variable for the item wanted
 options = Options()
---
+++ disk ps5 bots/Argos - disk.py
@@ -0,0 +1,29 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import winsound
+
+print("PS5 UK Bot Copyright (C) 2021  Mahdi Zaini This program comes with ABSOLUTELY NO WARRANTY; for details check included license. This is free software, and you are welcome to redistribute it under certain conditions; check license for conditions.")
+
+item = 'https://www.argos.co.uk/product/8349000' # Variable for the item wanted
+options = Options()
+options.add_argument("user-data-dir=C:\\Users\\ps5\\AppData\\Local\\Google\\Chrome\\User Data")
+driver = webdriver.Chrome('chromedriver.exe', options=options)
+driver.get(item)
+while True:
+    try:
+        # Code with possible error - error being that there's no add to cart button
+        driver.find_element_by_xpath('//*[@id="content"]/div/div/div[1]/div[3]/div[1]/section[2]/section/div[15]/div/div[2]/button').click() # Looks for the 'add to cart button' using xpath
+        driver.find_element_by_xpath('//*[@id="content"]/div/div/div[1]/div[3]/div[1]/section[2]/section/div[10]/div[3]/div[2]/div[5]/button').click() # Tries to atc as delivery
+    except:
+        driver.get(item)# If the atc button cannot be found, refresh until it is found
+    else:
+        time.sleep(1)
+    driver.get('https://www.argos.co.uk/basket') # Goes to basket
+    driver.find_element_by_xpath('//*[@id="basket-content"]/main/div[2]/section[3]/div[2]/div[2]/div/div/form/button').click() # Goes to checkout
+    break
+
+winsound.Beep(899, 5000) # Makes a beep sound for 5 seconds/5k ms to notify the user that the item is in stock and at checkout page
+
+
+
